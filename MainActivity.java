7738816f92269a9c7b61e51fefd8872f1ca51cd9
SHH1234-1 <84279445+SHH1234-1@users.noreply.github.com>
2022-02-25 00:15:33 +0900

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,117 +1,164 @@
-package com.example.myapp3;
+package com.example.mylottoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView zero;
-    TextView one;
-    TextView two;
-    TextView three;
-    TextView four;
-    TextView five;
-    TextView six;
-    TextView seven;
-    TextView eight;
-    TextView nine;
-    TextView ca;
-    TextView plus;
-    TextView result;
-
-    String newValue = "0";  <!--부호를 입력하기 전의 값-->
-    String oldValue = "0";  <!--부호를 입력하고 난 후에 입력할 값-->
+    private NumberPicker numberPicker;
+    private Button addButton;
+    private Button initButton;
+    private Button runButton;
+    private Set<Integer> pickerNumberSet = new HashSet<>();  //set생성
+    private ArrayList<TextView> textViews = new ArrayList<>();
+    private boolean didRun = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calculator_01);
-        initData();
+        setContentView(R.layout.activity_lotto);
+        initData();  <!--주소값이 담긴 메소드 호출-->
         addEventListener();
     }
 
     private void initData() {
-        zero = findViewById(R.id.zero);
-        one = findViewById(R.id.one);
-        two = findViewById(R.id.two);
-        three = findViewById(R.id.three);
-        four = findViewById(R.id.four);
-        five = findViewById(R.id.five);
-        six = findViewById(R.id.six);
-        seven = findViewById(R.id.seven);
-        eight = findViewById(R.id.eight);
-        nine = findViewById(R.id.nine);
-        ca = findViewById(R.id.ca);
-        plus = findViewById(R.id.plus);
-        result = findViewById(R.id.result);
+        numberPicker = findViewById(R.id.numberPicker);
+        numberPicker.setMinValue(1); //numberPicker의 최소값을 1
+        numberPicker.setMaxValue(45); //numberPicker의 최대값을 45
+
+        addButton = findViewById(R.id.addButton);  //초기화
+        initButton = findViewById(R.id.initButton);
+        runButton = findViewById(R.id.runButton);
+
+        textViews.add(findViewById(R.id.textView1));
+        textViews.add(findViewById(R.id.textView2));
+        textViews.add(findViewById(R.id.textView3));
+        textViews.add(findViewById(R.id.textView4));
+        textViews.add(findViewById(R.id.textView5));
+        textViews.add(findViewById(R.id.textView6));
     }
 
     private void addEventListener() {
-        zero.setOnClickListener(v -> {
-            newValue = newValue + "0";
-            result.setText(newValue);
-        });
-        one.setOnClickListener(v -> {
-            newValue = newValue + "1";
-            result.setText(newValue);
-        });
-        two.setOnClickListener(v -> {
-            // 1 + 2 (기존에 사용했던 값을 가지고 오고 + 여기에서 들어가는 값 ( 2 )
-            // "1" + "2" = "12"
-            newValue = newValue + "2";
-            result.setText(newValue);
-        });
-        three.setOnClickListener(v -> {
-            newValue = newValue + "3";
-            result.setText(newValue);
-        });
-        four.setOnClickListener(v -> {
-            newValue = newValue + "4";
-            result.setText(newValue);
-        });
-        five.setOnClickListener(v -> {
-            newValue = newValue + "5";
-            result.setText(newValue);
-        });
-        six.setOnClickListener(v -> {
-            newValue = newValue + "6";
-            result.setText(newValue);
-        });
-        seven.setOnClickListener(v -> {
-            newValue = newValue + "7";
-            result.setText(newValue);
-        });
-        eight.setOnClickListener(v -> {
-            newValue = newValue + "8";
-            result.setText(newValue);
-        });
-        nine.setOnClickListener(v -> { 
-            newValue = newValue + "9";
-            result.setText(newValue);
+        addButton.setOnClickListener(v -> {
+
+            int selectedNumber = numberPicker.getValue();
+
+            if (didRun) {
+                Toast.makeText(this, "초기화 후에 시도해주세요", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (pickerNumberSet.size() >= 5) {
+                Toast.makeText(this, "번호는 5개까지만 선택 가능합니다.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (pickerNumberSet.contains(selectedNumber)) {
+                Toast.makeText(this, "이미 선택한 번호 입니다.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            TextView textView = textViews.get(pickerNumberSet.size());
+            textView.setVisibility(View.VISIBLE);
+            textView.setText(String.valueOf(selectedNumber));
+            textView.setBackground(setTextViewBackground(selectedNumber));
+            pickerNumberSet.add(selectedNumber);
         });
 
-        ca.setOnClickListener(v -> {
-            newValue = "0";  
-            oldValue = "0";   
-            result.setText(newValue);
-        }); 
-        plus.setOnClickListener(v -> {
-            // 연산
-            int number1 = Integer.parseInt(newValue); <!--newValue가 String 형이므로 int형으로 바꿔줌    -->
-            int number2 = Integer.parseInt(oldValue); //(""); --> number
-            int sum = (number1 + number2);
-            oldValue = String.valueOf(sum); <!--int형을 다시 문자열로 변환시켜줌-->
-            newValue = "0";
-            result.setText(oldValue);
+        initButton.setOnClickListener(v -> {
+            didRun = false;
+            pickerNumberSet.clear();
+            for (TextView tv : textViews) {
+                tv.setVisibility(View.GONE);
+            }
         });
-        result.setOnClickListener(v -> {});
+
+        runButton.setOnClickListener(v -> {
+
+            if (didRun) {
+                Toast.makeText(this, "초기화 후에 시도해 주세요",
+                        Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            List<Integer> list = getRandomNumber();
+            list.addAll(pickerNumberSet);
+            // 코드 결과 상태 ( 6개 난수가 저장 됨) --> ( list )
+            Collections.sort(list);
+            Log.d("TAG", list.toString());
+
+            for (int i = 0; i < list.size(); i++) {
+                textViews.get(i).setText(String.valueOf(list.get(i))); //
+                textViews.get(i).setVisibility(View.VISIBLE);
+                textViews.get(i).setBackground(setTextViewBackground(list.get(i)));
+            }
+
+            didRun = true;
+
+        });
     }
+
+    private void getRandomNumberTest() {
+        // 랜던 숫자를 만들어주는 클래스를 가져온다.
+        Random random = new Random();  //난수를 만드는 클래스 Random
+        ArrayList<Integer> list = new ArrayList<>();  //번호를 담을 list 생성
+        while (list.size() < 6) {   //6개의 번호 선택을 위한 반복문
+            int number = random.nextInt(45) + 1;  //random난수의 nextInt(45) 0~44까지 랜덤한 수+1 를 더한 값을 int number에 저장
+            if (list.contains(number)) {  //ex 첫번째수가 5 두번째수가 5즉 같은 수를 뽑는 경우는
+                continue;  //무시하고 다시 맨위로 보냄
+            }
+            list.add(number);  //중복수가 아니라면 list에 추가
+        }
+        Collections.sort(list);  //작은 순서부터 큰 순서로 자동 정렬
+        Log.d("TAG", list.toString());  //list가 integer형이므로 String형태로 바꿔서 출력
+    }
+
+    private List<Integer> getRandomNumber() {
+        ArrayList<Integer> numberList = new ArrayList<>();
+ 
+        // 사용자 선택한 번호 확인
+        Log.d("TAG", pickerNumberSet.toString());
+
+        for (int i = 1; i < 46; i++) {
+
+            if (pickerNumberSet.contains(i)) {
+                continue;
+            }
+
+            numberList.add(i);
+        }
+        Collections.shuffle(numberList);
+        Log.d("TAG", "number List " + numberList.subList(0, 6).toString());
+        return numberList.subList(0, (6 - pickerNumberSet.size()));
+    }
+
+    private Drawable setTextViewBackground(int number) {
+        Drawable drawable;
+        // Drawable Resource 가져오는 방법
+        // todo 1 ~ 45 까지 색상을 지정해 주세요!!!
+        if (number < 11) {
+            drawable = ContextCompat.getDrawable(this, R.drawable.round_background_1);
+        } else if (number < 21) {
+            drawable = ContextCompat.getDrawable(this, R.drawable.round_background_2);
+        } else {
+            drawable = ContextCompat.getDrawable(this, R.drawable.round_background_1);
+        }
+        return drawable;
+    }
+
+
 }
-
-
-
-
